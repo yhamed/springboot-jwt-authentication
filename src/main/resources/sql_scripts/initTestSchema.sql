c146cb6950ddaccefe8ad6f955d97a1de5126cf1
Hamed Younes <hamed.younes@esprit.tn>
2023-03-18 13:41:45 +0100

    [FIX] re-organize and fix config for Integration tests SQL script

--- src/main/resources/sql_scripts/initTestSchema.sql
+++ src/main/resources/sql_scripts/initTestSchema.sql
@@ -14,19 +14,12 @@ CREATE TABLE test.roles
     name character varying(20)
 );
 
-ALTER TABLE test.roles OWNER TO postgres;
-
 CREATE SEQUENCE test.roles_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE CACHE 1;
-
-
-ALTER TABLE test.roles_id_seq OWNER TO postgres;
-
-ALTER SEQUENCE test.roles_id_seq OWNED BY test.roles.id;
 
 CREATE TABLE test.user_roles
 (
@@ -42,15 +35,21 @@ CREATE TABLE test.users
     username character varying(20)
 );
 
-ALTER TABLE test.users OWNER TO postgres;
-
 CREATE SEQUENCE test.users_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE CACHE 1;
 
+ALTER SEQUENCE test.roles_id_seq OWNED BY test.roles.id;
+
 ALTER TABLE test.users_id_seq OWNER TO postgres;
+
+ALTER TABLE test.roles_id_seq OWNER TO postgres;
+
+ALTER TABLE test.roles OWNER TO postgres;
+
+ALTER TABLE test.users OWNER TO postgres;
 
 ALTER SEQUENCE test.users_id_seq OWNED BY test.users.id;
 
@@ -62,10 +61,10 @@ ALTER TABLE ONLY test.roles
     ADD CONSTRAINT roles_pkey PRIMARY KEY (id);
 
 ALTER TABLE ONLY test.users
-    ADD CONSTRAINT uk6dotkott2kjsp8vw4d0m25fb7 UNIQUE (email) ON CONFLICT DO NOTHING;
+    ADD CONSTRAINT uk6dotkott2kjsp8vw4d0m25fb7 UNIQUE (email);
 
 ALTER TABLE ONLY test.users
-    ADD CONSTRAINT ukr43af9ap4edm43mmtq01oddj6 UNIQUE (username) ON CONFLICT DO NOTHING;
+    ADD CONSTRAINT ukr43af9ap4edm43mmtq01oddj6 UNIQUE (username);
 
 ALTER TABLE ONLY test.user_roles
     ADD CONSTRAINT user_roles_pkey PRIMARY KEY (user_id, role_id);
@@ -74,12 +73,15 @@ ALTER TABLE ONLY test.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
 ALTER TABLE ONLY test.user_roles
-    ADD CONSTRAINT fkh8ciramu9cc9q3qcqiv4ue8a6 FOREIGN KEY (role_id) REFERENCES test.roles(id) ON CONFLICT DO NOTHING;
+    ADD CONSTRAINT fkh8ciramu9cc9q3qcqiv4ue8a6 FOREIGN KEY (role_id) REFERENCES test.roles(id);
 
 ALTER TABLE ONLY test.user_roles
-    ADD CONSTRAINT fkhfh9dx7w3ubf1co1vdev94g3f FOREIGN KEY (user_id) REFERENCES test.users(id) ON CONFLICT DO NOTHING;
+    ADD CONSTRAINT fkhfh9dx7w3ubf1co1vdev94g3f FOREIGN KEY (user_id) REFERENCES test.users(id);
+
 
 INSERT INTO roles(id, name)
 VALUES (1, 'ROLE_USER') ON CONFLICT DO NOTHING;
 INSERT INTO roles(id, name)
 VALUES (2, 'ROLE_ADMIN') ON CONFLICT DO NOTHING;
+
+
